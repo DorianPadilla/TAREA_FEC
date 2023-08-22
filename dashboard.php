5b5b0bc46324ea933c89beb550c74a5100f160cc
dorian <25dorian05@gmail.com>
2023-08-22 08:43:24 -0500

    Agregando actualizaciones al dashboard

--- dashboard.php
+++ dashboard.php
@@ -22,69 +22,79 @@
             <nav class="navegation">
                 <button class="button">
                     <ion-icon name="home-outline"></ion-icon>
-                    <span> <a href="#">Dashboard</a>
-                </button>
-
-
-                <button class="button">
-                    <ion-icon name="person-outline"></ion-icon>
-                    <span> <a href="#">Customers</a>
+                    <span>
+                        <a href="dashboard.php"> Dashboard</a>
                     </span>
                 </button>
 
-
+                <button class="button">
+                    <ion-icon name="person-outline"></ion-icon>
+                    <span id="corte"> Cortes</span>
+                </button>
 
                 <button class="button">
                     <ion-icon name="home-outline"></ion-icon>
                     <span> <a href="#">Messages</a></span>
                 </button>
 
-
-
                 <button class="button">
                     <ion-icon name="help-circle-outline"></ion-icon>
                     <span> <a href="#">help</a></span>
                 </button>
-
-
 
                 <button class="button">
                     <ion-icon name="settings-outline"></ion-icon>
                     <span> <a href="#">settings</a></span>
                 </button>
 
-
-
                 <button class="button">
                     <ion-icon name="lock-closed-outline"></ion-icon>
                     <span> <a href="#">password</a></span>
                 </button>
 
-
-
                 <button class="button">
                     <ion-icon name="log-out-outline"></ion-icon>
-                    <span> <a href="#">Sing Out</a></span>
+                    <span id="cerrar-sesion"> <a href="#">Sing Out</a></span>
                 </button>
-
-
             </nav>
         </aside>
+        <section class="contenido">
+            <article class="cortes">
+                <div class="corte">
+                    <h1>Corte Diario</h1>
+                    <p id="corte-diario">20.200000</p>
+                </div>
+
+                <div class="corte">
+                    <h1>Corte Semanal</h1>
+                    <p id="corte-semanl">20.200000</p>
+                </div>
+                <div class="corte">
+                    <h1 id="corte-total">Corte Total</h1>
+                    <p> 53.223000</p>
+                </div>
+            </article>
+        </section>
         <nav id="navbar">
             <button class="button">
                 <ion-icon name="rose-outline"></ion-icon>
                 <span> <a href="#"></a></span>
             </button>
-            <div class="img">
-                <img src="img\3.jpg" alt="USER">
-            </div>
+            <button class="img">
+                <ion-icon name="person-circle-outline"></ion-icon>
+                <span><a href="#"></a></span>
+            </button>
         </nav>
+
     </div>
 
 
 
-
-    <script src=" js/dashboard.js"></script>
+    <script src=" js/dashboard.js">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
--- js/dashboard.js
+++ js/dashboard.js
@@ -2,10 +2,20 @@ const logo = document.getElementById('logo');
 const aside =document.querySelector('.aside')
 const spans = document.querySelectorAll('span');
 
-
 logo.addEventListener('click',()=>{
     aside.classList.toggle('-aside');
     spans.forEach((span)=>{
         span.classList.toggle('oculto');
     });
 }); 
+const card = document.querySelectorAll('.card');
+
+card.addEventListener('visibilitychange', () => {
+  if (card.hidden) {
+    // El div de clase card ha desaparecido
+    console.log('El div ha desaparecido');
+  } else {
+    // El div de clase card ha aparecido
+    console.log('El div ha aparecido');
+  }
+});
