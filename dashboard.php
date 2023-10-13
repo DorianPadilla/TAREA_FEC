123960204e8b0a818bc8c0c7165e607755bafe97
dorian <25dorian05@gmail.com>
2023-10-13 15:18:31 -0500

    prueba

--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,18 @@
 <div id="container">
     <?php require_once "Include/Header.php"; ?>
 
-    <section class="contenido">
-        <article class="cortes">
-            <div class="corte">
-                <h1>Corte Diario</h1>
-                <p id="corte-diario">20.200000</p>
-            </div>
-
-            <div class="corte">
-                <h1>Corte Semanal</h1>
-                <p id="corte-semanl">20.200000</p>
-            </div>
-            <div class="corte">
-                <h1 id="corte-total">Corte Total</h1>
-                <p> 53.223000</p>
-            </div>
-        </article>
+    <section class="corte">
+        <div class="corte-diario">
+            <h1>Corte Diario</h1>
+            <p> 20000000</p>
+        </div>
+        <div class="corte-semanal">
+            <h1>Corte semanal</h1>
+            <p>20000000</p>
+        </div>
+        <div class="corte-mensual">
+            <h1>Corte Mensual</h1>
+            <p> 20000000</p>
+        </div>
     </section>
 </div>
